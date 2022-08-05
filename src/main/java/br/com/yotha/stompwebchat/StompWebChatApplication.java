704d6fd5d89d985259c7c9ac2a4eba69c874a116
Marlon Divino <marlon@yotha.com.br>
2022-08-05 17:28:29 -0300

    Initial commit

---
+++ src/main/java/br/com/yotha/stompwebchat/StompWebChatApplication.java
@@ -0,0 +1,13 @@
+package br.com.yotha.stompwebchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompWebChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StompWebChatApplication.class, args);
+	}
+
+}
